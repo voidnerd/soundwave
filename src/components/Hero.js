a7cafa6f9fc4ceca1b9602579e1b5485ed847805
Ndifreke Friday <ndiecodes@gmail.com>
2019-09-22 16:14:53 +0100

    add discovery section

---
+++ src/components/Hero.js
@@ -0,0 +1,30 @@
+import React from 'react';
+import girl from '../assets/landing-page-girl.png';
+
+const Hero = () => {
+  return (
+    <>
+    <div className="flex px-2 md:px-20 pt-10 hero">
+
+        <div className="flex-wrap sm:flex-shrink-0 w-full md:w-1/2 min-h-full self-end girl">
+            <img src={girl} className="w-7/12 " />
+        </div>
+        <div class="flex-wrap flex-shrink-0  w-full md:w-1/2 min-h-full pt-20">
+            <div className="m-auto">
+                <h1 className="text-white text-6xl">Feel the music</h1>
+                <p className="text-white text-2xl font-normal">Stream over 20 thousand songs with one click</p>
+                <button className="bg-secondary hover:bg-inverse text-white font-bold font-poppins py-2 px-4 rounded mt-10">Join Now</button>
+            </div>
+        </div>
+
+        <div className="circle-1 bg-eclipes-pink"></div>
+        <div className="circle-2 bg-eclipes-blue"></div>
+        <div className="circle-3 bg-eclipes-blue"></div>
+       
+    </div>
+    <hr className="h-1 bg-inverse border-0"  />
+    </>
+  );
+}
+
+export default Hero;
